5ac19e443be635e9fa79a82178cf0f0588a8dbd7
evanpascual <pascual.evan@gmail.com>
2022-06-17 14:24:34 -0700

    Initial Commit

---
+++ firstSwiftApp/firstSwiftAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  firstSwiftAppApp.swift
+//  firstSwiftApp
+//
+//  Created by Evan Pascual on 6/17/22.
+//
+
+import SwiftUI
+
+@main
+struct firstSwiftAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
