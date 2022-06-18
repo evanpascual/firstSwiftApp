75856fd3d702f9901ee78f219b8ce5269bd8f1b5
evanpascual <pascual.evan@gmail.com>
2022-06-17 18:16:45 -0700

    edit UI

--- firstSwiftApp/ContentView.swift
+++ firstSwiftApp/ContentView.swift
@@ -9,13 +9,42 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        HStack() {
+            
+            Image("logo")
+                .resizable()
+                .aspectRatio(contentMode:
+                        .fit)
+                .padding()
+                .frame(width: 75.0, height: 75.0)
+                
+            
+            VStack(alignment: .leading) {
+                Text("Star Trek Coffee")
+                    .font(.title3)
+                    .fontWeight(.bold)
+                    
+                
+                Text("The best coffee in the galaxy")
+                    .font(.subheadline)
+                    .fontWeight(.medium)
+                    .foregroundColor(Color("Accent"))
+            }
+            
+            
+            
+            Spacer()
+            
+        }
+        .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color(hue: 0.59, saturation: 0.143, brightness: 1.0)/*@END_MENU_TOKEN@*/)
+        
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewInterfaceOrientation(.portrait)
     }
 }
